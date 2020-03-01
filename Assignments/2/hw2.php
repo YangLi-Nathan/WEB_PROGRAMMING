167dc67cf8e5e745d4fd92456df650fc2c2ac14c
Yang Li <yangli.atlanta@gmail.com>
2020-03-01 12:10:56 -0500

    Update hw2.php

--- Assignments/2/hw2.php
+++ Assignments/2/hw2.php
@@ -6,15 +6,12 @@
 <div>
     Question 1:
     <?php
-    // require 'functions.php';
-
     function CharlieAteMyLunch()
     {
         function isBitten()
         {
             return rand(0, 1);
         }
-
         if (isBitten()) {
             return "Charlie ate my lunch!";
         } else {
@@ -30,7 +27,7 @@
 <div>
     Question 2:
     <form action="hw2.php" method="post">
-        Please enter an INTEGER:<input type="text" pattern="^[0-9]*$" name="name">
+        Please enter an INTEGER to display checker board:<input type="text" pattern="^[0-9]*$" name="name">
         <input type="submit">
     </form>
     <table width="400px" cellspacing="1px" cellpadding="1px" border="1px">
@@ -54,6 +51,7 @@
             echo "</tr>";
         }
         ?>
+    </table>
 </div>
 <!-- QUESTION 3-->
 <div>
@@ -93,43 +91,175 @@
     {
         sort($month);
         foreach ($month as $value) {
-            echo "SORTED foreach: $value<br />";
+            echo "SORTED foreach: $value </br>";
         }
     }
 
     ?>
 </div>
-
+<!-- QUESTION 4 -->
 <div>
-
-<!-- QUESTION 4-->
     Question 4:
+
+    (Please see table below: )
+    <table width="80%" cellspacing="1px" cellpadding="1px" border="1px">
+        <?php
+        $top_rest = array(
+            'Chama Gaucha - Average Cost' => '40.50',
+            'Aviva by Kameel - Average Cost' => '15.00',
+            'Bone\'s Restaurant - Average Cost' => '65.00',
+            'Umi Sushi Buckhead - Average Cost' => '40.50',
+            'Fandangles - Average Cost' => '30.00',
+            'Capital Grille - Average Cost' => '60.50',
+            'Canoe - Average Cost Cost' => '35.50',
+            'One Flew South - Average Cost' => '21.00',
+            'Fox Bros.BBQ - Average Cost' => '15.00',
+            'South City Kitchen Midtown - Average Cost' => '29.00'
+        );
+
+        echo '<th>'. 'Restaurant Name'.'</th>';
+        echo '<th>'. 'Average Price'.'</th>';
+
+        // for loop to print out all the restaurant name and average price
+        foreach($top_rest as $key => $value){
+            echo '<tr>';
+            echo '<td>'.$key.'</td>';
+            echo '<td>'.'$'.$value.'</td><br>';
+            echo '</tr>';
+
+
+
+        }
+        // sort by price function
+        function sort_by_price($top_rest){
+            asort($top_rest);
+            foreach($top_rest as $key => $value){
+                echo '<tr>';
+                echo '<td>'.$key.'</td>';
+                echo '<td>'.'$'.$value.'</td><br>';
+                echo '</tr>';
+
+            }
+        }
+
+        echo '<th>'. 'Restaurant Name'.'</th>';
+        echo '<th>'. 'Sorted by Average Price'.'</th>';
+        sort_by_price($top_rest);
+
+
+        // sort by Name function
+        function sort_by_name($top_rest){
+            ksort($top_rest);
+            foreach($top_rest as $key => $value){
+                echo '<tr>';
+                echo '<td>'.$key.'</td>';
+                echo '<td>'.'$'.$value.'</td><br>';
+                echo '</tr>';
+
+            }
+        }
+
+        echo '<th>'. 'Sorted by Restaurant Name'.'</th>';
+        echo '<th>'. 'Average Price'.'</th>';
+        sort_by_name($top_rest);
+        ?>
+
+
+    </table>
+
+
+
+</div>
+<!-- QUESTION 5 -->
+<div>
+    <form action="hw2.php" method="post">
+        How many Hamburguer you have ordered? :
+        <input type="text" pattern="^[0-9]*$" name="name1">
+        <br>
+        How many Chocolate Milk Shake you have ordered? :
+        <input type="text" pattern="^[0-9]*$" name="name2">
+        <br>
+        How many Coke Cola you have ordered? :
+        <input type="text" pattern="^[0-9]*$" name="name3">
+        <br>
+        <input type="submit">
+
+    </form>
+
     <?php
+    # variables
+    $var1 = $_POST["name1"];
+    $var2 = $_POST["name2"];
+    $var3 = $_POST["name3"];
 
-    $names = array('Chama Gaucha - Average Cost $', 'Aviva by Kameel - Average Cost $',
-        'Bone\'s Restaurant - Average Cost $', 'Umi Sushi Buckhead - Average Cost $', 'Fandangles - Average Cost $',
-        'Capital Grille - Average Cost $', 'Canoe - Average Cost Cost $', 'One Flew South - Average Cost $',
-        'Fox Bros.BBQ - Average Cost $', 'South City Kitchen Midtown - Average Cost $');
+    $hamburguer=array('Hamburguer', 4.95, $var1);
+    $shake=array('Chocolate Milk', 1.95, $var2);
+    $soda=array('Coke', 0.85, $var3);
+    $tax_rate=0.075;
+    $tip_rate=0.16;
 
+    $subtotal = ($hamburguer[1] * $var1);
+    $subtotal = $subtotal + ($shake[1] * $var2);
+    $subtotal = $subtotal + ($soda[1] * $var3);
 
-    $price = array('40.50', '15.00', '65.00', '40.50', '30.00', '60.50', '35.50', '21.00', '15.00', '29.00');
-
-
-
-
-
-
-
+    $tax = $subtotal * $tax_rate;
+    $tip = $subtotal * $tip_rate;
+    $total = $subtotal + $tip + $tax;
 
     ?>
 
-
-
-
+    <table width="80%" border="1" cellspacing="0" cellpadding="4">
+        <tr>
+            <th>Item</th>
+            <th>Unit Price</th>
+            <th>Qty</th>
+            <th>Item Subtotal</th>
+        </tr>
+        <tr>
+            <td><?php echo "$hamburguer[0]";?></td>
+            <td><?php echo "$hamburguer[1]";?></td>
+            <td><?php echo "$hamburguer[2]";?></td>
+            <td><?php $ham_subtotal = ($hamburguer[1] * $var1);
+                print "\$$ham_subtotal"; ?></td>
+        </tr>
+        <tr>
+            <td><?php echo "$shake[0]";?></td>
+            <td><?php echo "$shake[1]";?></td>
+            <td><?php echo "$shake[2]";?></td>
+            <td><?php $sha_subtotal = ($shake[1] *$var2);
+                print "\$$sha_subtotal"; ?></td>
+        </tr>
+        <tr>
+            <td><?php echo "$soda[0]";?></td>
+            <td><?php echo "$soda[1]";?></td>
+            <td><?php echo "$soda[2]";?></td>
+            <td><?php $sod_subtotal = ($soda[1] * $var3);
+                print "\$$sod_subtotal"; ?></td>
+        </tr>
+        <tr>
+            <td colspan="3">&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td colspan="3" align="right">Subtotal</td>
+            <td><?php echo "\$$subtotal"; ?></td>
+        </tr>
+        <tr>
+            <td colspan="3" align="right">Tax, 7.5%</td>
+            <td><?php echo "\$$tax"; ?></td>
+        </tr>
+        <tr>
+            <td colspan="3" align="right">Tip</td>
+            <td><?php echo "\$$tip"; ?></td>
+        </tr>
+        <tr>
+            <th colspan="3" align="right">Total</th>
+            <td><?php echo "\$$total"; ?></td>
+        </tr>
+    </table>
 
 </div>
 
 
 </body>
-
 </html>
