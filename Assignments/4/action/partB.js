f21523d8ca6a6703220c018dbee3b403a6e43ec6
Yang Li <yangli.atlanta@gmail.com>
2020-03-23 14:45:17 -0400

    partB completed

--- Assignments/4/action/partB.js
+++ Assignments/4/action/partB.js
@@ -31,18 +31,20 @@ function checkNum() {
         message.textContent = "Enter a higher number";
         chance.textContent = "You have " + guessLimit + " left.";
     } else if (userNum == targetNum) {
-        message.textContent = "You got it right!";
+        winAudio();
+        message.textContent = "Bingo!!!";
+        chance.textContent = "";
         gameContinues = false;
     }
-
     checkGameStatus();
 }
 
 // check if the game is end
-
 function checkGameStatus() {
     if (gameContinues == false) {
-        message.textContent = "Game is Over, Start a New game";
+        //message.textContent = "Game is Over, Start a New game";
+
+        aTimer.stop();
         gameOver();
     }
     if (guessLimit == 0) {
@@ -55,6 +57,9 @@ function checkGameStatus() {
 }
 
 function newGame() {
+    aTimer.reset();
+    aTimer.start();
+
     message.textContent =
         "A New Game Started, you will have 10 chances to guess the number!";
     btn.parentNode.removeChild(btn);
@@ -73,3 +78,91 @@ function gameOver() {
     document.body.appendChild(btn);
     btn.addEventListener("click", newGame);
 }
+// audio sound effects
+
+function playAudio() {
+    document.getElementById("clockAudio").play();
+}
+
+function pauseAudio() {
+    document.getElementById("clockAudio").pause();
+}
+
+function winAudio() {
+    document.getElementById("winAudio").play();
+}
+
+// stopwatch
+
+var Stopwatch = function(elem, options) {
+    var timer = createTimer(),
+        offset,
+        clock,
+        interval;
+
+    // default options
+    options = options || {};
+    options.delay = options.delay || 1;
+
+    elem.appendChild(timer);
+
+    // initialize
+    reset();
+
+    // private functions
+    function createTimer() {
+        return document.createElement("span");
+    }
+
+    function start() {
+        playAudio();
+        if (!interval) {
+            offset = Date.now();
+            interval = setInterval(update, options.delay);
+        }
+    }
+
+    function stop() {
+        pauseAudio();
+        if (interval) {
+            clearInterval(interval);
+            interval = null;
+        }
+    }
+
+    function reset() {
+        clock = 0;
+        render(0);
+    }
+
+    function update() {
+        clock += delta();
+        render();
+    }
+
+    function render() {
+        timer.innerHTML = clock / 1000;
+    }
+
+    function delta() {
+        var now = Date.now(),
+            d = now - offset;
+
+        offset = now;
+        return d;
+    }
+
+    this.start = start;
+    this.stop = stop;
+    this.reset = reset;
+};
+
+var elems = document.getElementsByClassName("basic");
+
+for (var i = 0, len = elems.length; i < len; i++) {
+    new Stopwatch(elems[i]);
+}
+
+var a = document.getElementById("a-timer");
+aTimer = new Stopwatch(a);
+aTimer.start();
